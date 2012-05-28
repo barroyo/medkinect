e3f4d5ed2805225592d187ec129d258458dcb231
Cristopher Araya <caraya@avantica.net>
2012-05-28 17:01:41 -0600

    adding server

---
+++ server/medkinect/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Medkinect::Application.config.secret_token = 'cb4bf84e308a19938119d73b2a2108862bc281a362880b8b9d40c803d7e72ee80fdcf31f55b4f20345a367228415291f7a4954a3499273d43c1fa9043343bd9a'
