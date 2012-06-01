a5ab4fe7cdbdff53076a7a2d149dda05ba7efb6e
Cristopher Araya <araya.luster@gmail.com>
2012-05-31 23:00:54 -0600

    adding rails with 2 controllers, user and roles

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-
+    @user.role = Role.find(@user.id)
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user }
