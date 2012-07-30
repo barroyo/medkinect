ad3cdc7b77914b3db55f7365498a5de8283bfef8
Cristopher Araya <araya.luster@gmail.com>
2012-07-29 21:35:14 -0600

    adding client side validations, changes into models

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -7,4 +7,7 @@ class User < ActiveRecord::Base
   validates_length_of :password, :minimum => 4, :allow_blank => true
   validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => "Invalid email format"
   validates_format_of :username, :with => /\A[a-zA-Z]+([a-zA-Z]|\d)*\Z/, :message => "Invalid no numbers or special characters allowed"
+  def self.authenticate(username, password)
+		find(:first,:conditions => [ "username = '%s' AND password = '%s' and status = 'en'", username, Digest::MD5.hexdigest(password)])
+  end
 end
