d98fb6d1f036650dd5e08aa13bcb6afbceb1279a
diegojimenezdelgado <diego.jimenez33@dotcreek.com>
2012-09-11 11:27:45 -0500

    upload latest changes

---
+++ API/API-MEDKINECT/API-MEDKINECT/Calendars.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_MEDKINECT
+{
+     public  class Calendars
+        {
+            ApiMedkinect apiMedkinect = new ApiMedkinect();
+            public Calendars() { }
+
+            public int id;
+            public int user_id;
+            public int patient_id;
+            public DateTime date;
+            public string date_type;
+            public string result;
+            public string status;
+          
+
+            public Object update_patients()
+            {
+                Object result;
+                result = apiMedkinect.conexion_rest("put", "api/calendars", this, this.id);
+                return result;
+            }
+
+            public Object create_patients()
+            {
+                Object result;
+                result = apiMedkinect.conexion_rest("post", "api/calendars", this, 0);
+                return result;
+            }
+
+            public Object delete_patients()
+            {
+                Object result;
+                result = apiMedkinect.conexion_rest("delete", "api/calendars", null, this.id);
+                return result;
+            }
+
+            public Object get_patients()
+            {
+                Object result;
+                result = apiMedkinect.conexion_rest("get", "api/calendars", null, this.id);
+                return result;
+            }
+
+            public Object get_all_patients()
+            {
+                Object result;
+                result = apiMedkinect.conexion_rest("getall", "api/calendars", null, 0);
+                return result;
+            }
+        }
+}
---
+++ API/API-MEDKINECT/API-MEDKINECT/Roles.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_MEDKINECT
+{
+    public class Roles 
+    {
+        ApiMedkinect apiMedkinect = new ApiMedkinect();
+        public Roles() { }
+
+        public int id;
+        public string title;
+        public string description;
+        public string e;
+        public string access;
+        public int user_id;
+
+
+        public Object update_roles()
+        {
+            Object result;
+            result = apiMedkinect.conexion_rest("put", "api/roles", this, this.id);
+            return result;
+        }
+
+        public Object create_roles()
+        {
+            Object result;
+            result = apiMedkinect.conexion_rest("post", "api/roles", this, 0);
+            return result;
+        }
+
+        public Object delete_roles()
+        {
+            Object result;
+            result = apiMedkinect.conexion_rest("delete", "api/roles", null, this.id);
+            return result;
+        }
+
+        public Object get_roles()
+        {
+            Object result;
+            result = apiMedkinect.conexion_rest("get", "api/roles", null, this.id);
+            return result;
+        }
+
+        public Object get_all_roles()
+        {
+            Object result;
+            result = apiMedkinect.conexion_rest("getall", "api/roles", null, 0);
+            return result;
+        }
+    }
+}
