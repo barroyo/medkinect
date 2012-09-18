45721058bbc7b06dcdcf38660768ef6a836a6c44
Cristopher Araya <caraya@avantica.net>
2012-09-17 22:48:59 -0600

    style, wrong spelling and validations

--- server/app/controllers/home_controller.rb
+++ server/app/controllers/home_controller.rb
@@ -3,7 +3,13 @@ class HomeController < ApplicationController
 	before_filter :require_login
 	
   def index
-  	@twi = Twitter.user_timeline("medkinect").first(5)
+  	@twi = []
+  	begin
+  		@twi = Twitter.user_timeline("medkinect").first(5)
+  	rescue Exception => e
+  		flash[:notice] = e.message
+  		flash[:class_css] = 'error'
+  	end
   	respond_to do |format|
   		format.html
   		format.json {render json: @twi}
